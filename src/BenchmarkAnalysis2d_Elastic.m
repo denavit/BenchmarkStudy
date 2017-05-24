e1cc3acefd51b089a452b5719744513e82e1e589
denavit <denavit@gmail.com>
2017-05-24 09:31:45 -0400

    Adding an option to set EI when defining elastic benchmark frame

--- src/BenchmarkAnalysis2d_Elastic.m
+++ src/BenchmarkAnalysis2d_Elastic.m
@@ -1,4 +1,13 @@
-function obj = BenchmarkAnalysis2d_Elastic(data)
+function obj = BenchmarkAnalysis2d_Elastic(data,EI)
+if nargin > 1
+    if isnumeric(EI)
+        data.EI = EI;
+    elseif ischar(EI)
+        data.EI = data.section.EI(data.axis,EI);
+    else
+        error('Unknown EI type: %s',type(EI));
+    end
+end
 switch data.frame_type
     case 'Sidesway_Uninhibited'
         obj = BenchmarkAnalysis2d_Elastic_Sidesway_Uninhibited(data);
