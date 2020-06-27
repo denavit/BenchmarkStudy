ee714fd7d59145f59c03096bb991e2b7f14242ca
denavit <denavit@gmail.com>
2020-06-27 15:15:48 -0400

    Create create_benchmark_study.m

---
+++ RC/create_benchmark_study.m
@@ -0,0 +1,39 @@
+clear all; close all; clc;
+
+study_name = 'RC';
+study_path = fullfile(pathOf.BenchmarkStudyResults,study_name);
+
+create_study    = false;
+build_data      = true;
+build_options   = true;
+
+%% Create Study
+if create_study
+    study = BenchmarkStudy('create',study_path,study_name);
+end
+
+%% Build Data
+if build_data
+    study = BenchmarkStudy('open',study_path);
+    data = build_data_RC();
+    save(study.path_of_data(),'data')
+end
+
+%% Build Options
+if build_options
+    study = BenchmarkStudy('open',study_path);
+
+    % Define fiber_section_definition_options
+    fiber_section_definition_options = struct;
+    fiber_section_definition_options.nf1 = 30;
+    fiber_section_definition_options.includePackageDefinition = true;
+    fiber_section_definition_options.ConcreteMaterialType = 'Concrete04';
+    fiber_section_definition_options.ReinforcementMaterialType = 'ElasticPP';
+    study.set_fiber_section_definition_options('Strength',fiber_section_definition_options)
+    
+    % Define analysis_options
+    analysis_options = struct;    
+    analysis_options.absoluteStrainLimit = 0.05;
+    study.set_analysis_options('Strength',analysis_options)
+end
+    
