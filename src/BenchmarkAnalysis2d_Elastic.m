6e41b005dbcbdab3766df82bf19cbada0c60c659
denavit <denavit@gmail.com>
2020-10-09 17:24:22 -0400

    Adding if statement to avoid errors

--- src/BenchmarkAnalysis2d_Elastic.m
+++ src/BenchmarkAnalysis2d_Elastic.m
@@ -5,12 +5,16 @@ if nargin > 1
     elseif ischar(EI)
         if strcmpi(EI,'RC_Study_With_Stiffness_Reduction')
             data.EI = 0.7*data.section.Ec*data.section.Ig(data.axis);
-            data.kqtop = 6*0.35*data.EcIgb_over_Lb;
-            data.kqbot = 6*0.35*data.EcIgb_over_Lb;
+            if strcmpi(data.frame_type,'Sidesway_Uninhibited')
+                data.kqtop = 6*0.35*data.EcIgb_over_Lb;
+                data.kqbot = 6*0.35*data.EcIgb_over_Lb;
+            end
         elseif strcmpi(EI,'RC_Study_Without_Stiffness_Reduction')
             data.EI = 0.8*data.section.Ec*data.section.Ig(data.axis);
-            data.kqtop = 6*0.4*data.EcIgb_over_Lb;
-            data.kqbot = 6*0.4*data.EcIgb_over_Lb;            
+            if strcmpi(data.frame_type,'Sidesway_Uninhibited')
+                data.kqtop = 6*0.4*data.EcIgb_over_Lb;
+                data.kqbot = 6*0.4*data.EcIgb_over_Lb;
+            end
         else
             data.EI = data.section.EI(data.axis,EI);
         end
